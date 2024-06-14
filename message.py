864bc18021ddda8f22a186a90ead3b234dba1494
Luan Petruitis <luanpetruitis@hotmail.com>
2024-06-14 10:09:06 -0300

    Integrando com OpenAI

---
+++ message.py
@@ -0,0 +1,30 @@
+# pip install openai==0.28
+
+import openai
+import chatGPT
+
+openai.api_key = chatGPT.api_key
+
+# Define the model engine
+model_engine = "gpt-3.5-turbo"
+
+# Get user input
+teste = input("Faça sua pergunta: ")
+
+# Create a completion using the new API method
+response = openai.ChatCompletion.create(
+    model=model_engine,
+    messages=[
+        {"role": "system", "content": "Você é um assistente de IA."}, #  passar para o chat gpt o que você quer que ele faça
+        # {"role": "assistant", "content": "O que o chat GPT ira responder"},
+        {"role": "user", "content": teste} #  o que o usuário está perguntando
+    ],
+    max_tokens=1024,
+    temperature=0.7
+)
+
+# Print the response
+if response is not None and response.choices is not None:
+    print("A resposta encontrada foi:")
+    for choice in response.choices:
+        print(choice.message["content"])
